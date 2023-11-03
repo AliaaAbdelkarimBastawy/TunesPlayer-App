691e6e6c38aa50e63ea06f22778371f8c113bad1
Aliaa <70468590+lola750@users.noreply.github.com>
2023-11-03 16:51:15 +0200

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tune_app/views/TunesPage.dart';
+import 'package:tune_app/views/TunesView.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false,
